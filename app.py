52b1bc4804a6d1d14ac2ddde710e21bee241bc02
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-28 20:32:00 +0900

    add delete sample

--- app.py
+++ app.py
@@ -61,11 +61,33 @@ def update_heroes():
 
         print("Updated hero:", hero)
 
+
+def delete_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        results = session.exec(statement)
+        hero = results.one()
+        print("Hero:", hero)
+
+        session.delete(hero)
+        session.commit()
+
+        print("Deleted hero:", hero)
+
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        results = session.exec(statement)
+        hero = results.first()
+
+        if hero is None:
+            print("There's no hero named Spider-Youngster")
+
+
 def main():
     create_db_and_tables()
     create_heroes()
     select_heroes()
     update_heroes()
+    delete_heroes()
 
 
 if __name__ == "__main__":
