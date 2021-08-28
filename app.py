e855f6e263a805abf449d05d36b880bcc79cc2c5
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-28 15:35:54 +0900

    add heroes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel, create_engine, Session, select
+from sqlmodel import Field, SQLModel, create_engine, Session, select, or_
 
 
 class Hero(SQLModel, table=True):
@@ -24,18 +24,25 @@ def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
-        session = Session(engine)
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
+        statement = select(Hero).where(or_(Hero.age <= 35, Hero.age > 90))
         results = session.exec(statement)
         for hero in results:
             print(hero)
