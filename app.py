63756a2a5ab2508f73bc7a5146c739343a0953d8
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-28 20:24:39 +0900

    add update sample

--- app.py
+++ app.py
@@ -46,10 +46,26 @@ def select_heroes():
         results = session.exec(statement).all()
         print(results)
 
+
+def update_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        results = session.exec(statement)
+        hero = results.one()
+        print("Hero:", hero)
+
+        hero.age = 16
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+
+        print("Updated hero:", hero)
+
 def main():
     create_db_and_tables()
     create_heroes()
     select_heroes()
+    update_heroes()
 
 
 if __name__ == "__main__":
