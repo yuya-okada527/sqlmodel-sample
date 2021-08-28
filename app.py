08f6c1585dcd8f11dae57322dbee0c1dee1a0a5f
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-28 21:13:39 +0900

    add hero teams

--- app.py
+++ app.py
@@ -90,12 +90,47 @@ def delete_heroes():
             print("There's no hero named Spider-Youngster")
 
 
+def create_heroes():
+    with Session(engine) as session:
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sister Margaret's Bar")
+        session.add(team_preventers)
+        session.add(team_z_force)
+        session.commit()
+
+        hero_deadpond = Hero(
+            name="Deadpond",
+            secret_name="Dive Wilson",
+            team_id=team_z_force.id
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man",
+            secret_name="Tommy Sharp",
+            age=48,
+            team_id=team_preventers.id
+        )
+        hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Parquedor")
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Created hero:", hero_deadpond)
+        print("Created hero:", hero_rusty_man)
+        print("Created hero:", hero_spider_boy)
+
+
 def main():
     create_db_and_tables()
     # create_heroes()
     # select_heroes()
     # update_heroes()
     # delete_heroes()
+    create_heroes()
 
 
 if __name__ == "__main__":
